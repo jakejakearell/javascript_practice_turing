e38f378f5ff9f086404406889a18df3cc85f8531
Jacob Arellano <jmm.arellano@gmail.com>
2021-10-15 13:24:34 -0600

    Finish Dragons

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
+class Dragon{
+    constructor(name, rider){
+        this.name = name
+        this.rider = rider
+        this.hungry = true 
+        this.fed = 0 
+    }
 
+    greet() {
+        return `Hi, ${this.rider}!`
+    }
+    
+    eat() {
+        if (this.fed > 1) {
+          this.hungry = false;
+        } else {
+          this.fed += 1;
+        }    
+    }
+}
+
+module.exports = Dragon;
